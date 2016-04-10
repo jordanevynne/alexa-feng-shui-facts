985fe9fb825ee045446288ca2bf1a2a98e259d8f
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-04-10 14:56:18 -0700

    added all feng shui facts

--- index.js
+++ index.js
@@ -27,19 +27,20 @@ var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-valu
  * Array containing feng shui facts.
  */
 var FENG_SHUI_FACTS = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+    "Feng shui is the ancient Chinese art of placement, and is used to faciliate flow of Chi.",
+    "Feng shui translates to wind and water, and together these words represent harmony and balance.",
+    "Feng shui has existed for over 3,000 years.",
+    "Blue and Black are associated with water energy, and they support inner work.",
+    "Green is associated with wood energy, and can motivate internal change.",
+    "Red is associated with fire, which supports life energy and can promote activity.",
+    "Yellow is associated with earth energy, and is thought to elevate mental activity.",
+    "White and metallic colors are thought to support focus and purity.",
+    "To create good feng shui in your bedroom, position your bed so that it is not in line with the door.",
+    "To create good feng shui in your bedroom, avoid placing your bed in front of or underneath a window.",
+    "To create good feng shui in your living room, avoid placing a mirror in a position where it reflects doors and windows.",
+    "To create good feng shui in your living room, place green plants in the south east area of the room.",
+    "To create good feng shui in your bathroom, open a window to let in fresh air.",
+    "To create good feng shui in your kitchen, it is recommended to have 9 oranges in a bowl for good luck and prosperity."
 ];
 
 /**
